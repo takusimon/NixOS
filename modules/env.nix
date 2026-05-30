534bb5510d28689ffce1891865faedbd0e2912d2
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-05-30 16:12:29 -0500

    added shell prompt for root account

--- modules/env.nix
+++ modules/env.nix
@@ -30,6 +30,9 @@
 		    shellAliases = {
 		    		 emacs = "emacs -nw";
 		    };
-
+		    variables = {
+		      PS1 = "'\[\e[38;5;220m\]\u\[\e[0m\]@\[\e[38;5;227m\]\H\[\e[0m\] \[\e[1;2m\]in\[\e[0m\] \[\e[38;5;39m\]\w\[\e[0m\] ${PS1_CMD1}\n\$ '";
+		      PROMPT_COMMAND = "PROMPT_COMMAND='PS1_CMD1=$(git branch --show-current 2>/dev/null)'";
+		    };
 	};
 }
