0a173dc9135dcbda5c9adcccb7a6b95be96d463d
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-06-14 20:03:31 -0500

    added tartarus grub theme and switched on plymouth for boot splash screen

--- modules/boot.nix
+++ modules/boot.nix
@@ -2,6 +2,17 @@
 	# Set Grub as Default Bootloader & Always use the latest kernel
 
 	boot = {
+	     consoleLogLevel = 3;
+	     initrd.verbose = false;
+	     initrd.systemd.enable = true;
+	     kernelParams = [
+	      "quiet"
+	      "splash"
+	      "intremap=on"
+	      "boot.shell_on_fail"
+	      "udev.log_priority=3"
+	      "rd.systemd.show_status=auto"
+	     ];
 	     loader = {
 	     	    grub = {
 		    	 enable = true;
@@ -9,6 +20,7 @@
 			 device = "nodev";
 			 timeoutStyle = "menu";
 			 configurationLimit = 10;
+			 theme = ../home/tartarus;
 		   };
 
 		   efi = {
@@ -16,6 +28,18 @@
 		       canTouchEfiVariables = true;
 		   };
 	    };
+
+	    plymouth = let
+	    plymouth-theme = pkgs.adi1090x-plymouth-themes.override {
+	     selected_themes = [ "unrap" ];
+	    };
+	    in
+	    {
+	     enable = true;
+	     themePackages = with pkgs; [ adi1090x-plymouth-themes ];
+	    };
+	     
+	     
 
 	    kernelPackages = pkgs.linuxPackages_latest;
       };
