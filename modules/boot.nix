89749f3c07ae6b17e7c594e41f6b004680cf499f
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-06-14 23:05:54 -0500

    added plymouth but have to work on timing issues current duration too short

--- modules/boot.nix
+++ modules/boot.nix
@@ -2,15 +2,28 @@
 	# Set Grub as Default Bootloader & Always use the latest kernel
 
 	boot = {
-	     consoleLogLevel = 3;
-	     initrd.verbose = false;
+	  plymouth =  
+	    {
+	     enable = true;
+	     themePackages = with pkgs; [
+	     (adi1090x-plymouth-themes.override {
+              selected_themes = [ "unrap" ];
+             })
+	     ];
+	     theme = "unrap";
+	     extraConfig = ''
+	      ShowDelay=0
+	      DeviceTimeout=8
+	    '';
+	    };
+	  consoleLogLevel = 3;
 	     initrd.systemd.enable = true;
+	     initrd.verbose = false;
 	     kernelParams = [
 	      "quiet"
 	      "splash"
-	      "intremap=on"
 	      "boot.shell_on_fail"
-	      "udev.log_priority=3"
+	      "rd.udev.log_priority=3"
 	      "rd.systemd.show_status=auto"
 	     ];
 	     loader = {
@@ -22,24 +35,12 @@
 			 configurationLimit = 10;
 			 theme = ../home/tartarus;
 		   };
-
 		   efi = {
 		       efiSysMountPoint = "/boot";
 		       canTouchEfiVariables = true;
 		   };
 	    };
 
-	    plymouth = let
-	    plymouth-theme = pkgs.adi1090x-plymouth-themes.override {
-	     selected_themes = [ "unrap" ];
-	    };
-	    in
-	    {
-	     enable = true;
-	     themePackages = with pkgs; [ adi1090x-plymouth-themes ];
-	    };
-	     
-	     
 
 	    kernelPackages = pkgs.linuxPackages_latest;
       };
--- modules/services.nix
+++ modules/services.nix
@@ -1,4 +1,4 @@
-{config,pkgs,...}: {
+{config,pkgs,lib,...}: {
     #Set system services sound, desktop manager, etc
     services = {
     	    displayManager = {
